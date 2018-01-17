1db6b9769d0ded96a5961b69c2b38d62bf70e921
Eli <turrinelias@gmail.com>
2018-01-17 18:42:53 -0300

    Primer clase

---
+++ Clase 01/Alumno/Ejercicio/index.js
@@ -0,0 +1,15 @@
+function User(firstName, age, isAdmin = false, telephones = []) {
+	this.addPhone = function (phone) {
+		telephones.push(phone)
+	}
+
+	this.showUser = function () {
+		console.log(`Nombre: ${firstName}, Edad: ${age}, Contacto: ${telephones}`)
+	}
+}
+
+const person = new User('Elias', 30, null, [123456])
+
+person.addPhone(654321)
+
+person.showUser()
